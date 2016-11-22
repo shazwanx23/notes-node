5d27682ed885ca1f71c818e1504e5084e133595a
shazwan <shazwanx23@gmail.com>
2016-11-22 22:36:29 +0800

    yargs

--- notes.js
+++ notes.js
@@ -1,6 +1,24 @@
 console.log('Starting notes.js');
 
-module.exports.addNote = () =>{
-	console.log('addNote');
-	return 'New note';
+var addNote = (title, body) =>{
+	console.log('Adding note', title, body);
+};
+
+var getAll = () =>{
+	console.log('Getting all notes');
+};
+
+var getNote = (title) => {
+	console.log('Getting note with title: ', title);
+};
+
+var remove = (title) => {
+	console.log('Removing note with title: ', title);
+};
+
+module.exports = {
+	addNote,
+	getAll,
+	getNote,
+	remove
 };
