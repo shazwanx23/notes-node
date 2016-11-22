2e2b9655895fd61cd84dcb9742055a7c21651a5a
Nor Shazwan Shah Nor Hisham Shah <shazwan.hisham@mimos.local>
2016-11-22 09:39:45 +0800

    remove junk

--- app.js
+++ app.js
@@ -1,18 +1,6 @@
 console.log('Starting app.js');
 
 const fs = require('fs');
-const os = require('os');
 const _ = require('lodash');
+
 const notes = require('./notes.js');
-
-// console.log(_.isString(true));
-// console.log(_.isString('Andrew'));
-var filteredArray = _.uniq(['shazwan',1,'shazwan', 1,2,3,4]);
-console.log(filteredArray);
-
-
-// console.log('Result: ', notes.add(9,-2));
-
-// var user = os.userInfo();
-
-// fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`);
