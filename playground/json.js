086ae9009339399172477a289090a3a6bd096d40
shazwan <shazwanx23@gmail.com>
2016-11-22 22:54:04 +0800

    create and read JSON

---
+++ playground/json.js
@@ -0,0 +1,27 @@
+// var obj = {
+// 	name: 'shazwan'
+// };
+// var stringObj = JSON.stringify(obj);
+// console.log(typeof stringObj);
+// console.log(stringObj);
+
+// var personString = '{"name": "Shazwan", "age": 25}';
+// var person = JSON.parse(personString);
+// console.log(typeof person);
+// console.log(person);
+
+const fs = require('fs');
+
+var originalNote = {
+	title: 'Some Title',
+	body: 'Some body'
+};
+//originalNoteString
+var originalNoteString = JSON.stringify(originalNote);
+fs.writeFileSync('notes.json', originalNoteString);
+
+var noteString = fs.readFileSync('notes.json');
+var note = JSON.parse(noteString);
+
+console.log(typeof note);
+console.log(note.title);
