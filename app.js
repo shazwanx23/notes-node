ed8345ac4f99cf56fca4716dbfc47e81588e7056
Nor Shazwan Shah Nor Hisham Shah <shazwan.hisham@mimos.local>
2016-11-25 16:29:12 +0800

    completed notes app

--- app.js
+++ app.js
@@ -1,15 +1,33 @@
-console.log('Starting app.js');
-
 const fs = require('fs');
 const _ = require('lodash');
 const yargs = require('yargs');
 
 const notes = require('./notes.js');
-
-const argv = yargs.argv;
+const titleOptions = {
+	describe: 'Title of note',
+	demand: true,
+	alias: 't'	
+};
+const bodyOptions = {
+	describe: 'Body of note',
+	demand: true,
+	alias: 'b'
+};
+const argv = yargs
+	.command('add', 'add a new note', {
+		title: titleOptions,
+		body: bodyOptions
+	})
+	.command('list', 'List all notes')
+	.command('read', 'Read a note', {
+		title: titleOptions
+	})
+	.command('remove', 'Remove a note', {
+		title: titleOptions
+	})
+	.help()
+	.argv;
 var command = argv._[0];
-console.log('Command: ', command);
-console.log('Yargs: ', argv);
 
 if (command === 'add'){
 	var note = notes.addNote(argv.title, argv.body);
@@ -20,7 +38,9 @@ if (command === 'add'){
 		console.log("Fail to add note. Duplicate title detected");
 	}
 } else if (command === 'list'){
-	notes.getAll();
+	var allNotes = notes.getAll();
+	console.log(`Printing ${allNotes.length} note(s).`)
+	allNotes.forEach((note) =>notes.logNote(note));
 } else if (command === 'read'){
 	var note = notes.getNote(argv.title);
 	if(note){
